f3ab06b9f03612107c8091aedc1c93e8b3fc3121
KimRoks <113083860+KimRoks@users.noreply.github.com>
2024-02-29 18:10:29 +0900

    [Silver III] Title: 1로 만들기, Time: 20 ms, Memory: 76916 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.swift
@@ -0,0 +1,14 @@
+let n = Int(readLine()!)!
+if n == 1 {
+    print(0)
+} else {
+    var dp = Array(repeating: 0, count: n+1)
+
+    for i in 2...n {
+        dp[i] = dp[i-1] + 1
+        if i % 2 == 0 { dp[i] = min(dp[i], dp[i/2] + 1) }
+        if i % 3 == 0 { dp[i] = min(dp[i], dp[i/3] + 1) }
+    }
+
+    print(dp[n])
+}
