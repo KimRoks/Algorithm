7417dc06dd3ae3b2398f62b6e6f239008abbba95
KimRoks <113083860+KimRoks@users.noreply.github.com>
2024-08-24 22:17:54 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+var input = readLine()!.split(separator: " ").map{ Int($0)!}
+
+// 무조건 1개는 있으니까
+var dp = Array(repeating: 1, count: n)
+
+for i in 0..<n {
+    for j in 0..<i {
+        if input[j] < input[i] {
+            dp[i] = max(dp[i], dp[j] + 1)
+        }
+    }
+}
+
+print(dp.max()!)
