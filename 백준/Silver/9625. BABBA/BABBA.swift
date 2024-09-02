d701f0f74feb35c51a8ed72ceb80b8973e134303
KimRoks <113083860+KimRoks@users.noreply.github.com>
2024-09-02 18:53:55 +0900

    [Silver V] Title: BABBA, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Silver/9625. BABBA/BABBA.swift
+++ 백준/Silver/9625. BABBA/BABBA.swift
@@ -1,15 +1,14 @@
 let k = Int(readLine()!)!
 
-var acount = 0
-var bcount = 0
-var dp = [(1,0), (0,1), (1,1), (1,2), (2,3)] + [(Int, Int)](repeating: (0, 0), count: 41)
+var aCount = 1
+var bCount = 0
 
-
-for i in 5..<dp.count {
-    dp[i] = (
-        dp[i - 1].0 + dp[i - 2].0,
-        dp[i - 2].1 + dp[i - 1].1
-    )
+for _ in 1...k {
+    let newACount = bCount
+    let newBCount = aCount + bCount
+    
+    aCount = newACount
+    bCount = newBCount
 }
 
-print(dp[k].0, dp[k].1)
+print(aCount, bCount)
