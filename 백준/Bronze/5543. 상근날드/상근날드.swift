fc4596596bf25669a68a64c6f4b76ae0aae28335
KimRoks <113083860+KimRoks@users.noreply.github.com>
2024-09-15 01:42:18 +0900

    [Bronze IV] Title: 상근날드, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.swift
@@ -0,0 +1,10 @@
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+let c = Int(readLine()!)!
+let d = Int(readLine()!)!
+let e = Int(readLine()!)!
+
+let minBuger = min(a, b, c)
+let minJuice = min(d,e)
+
+print(minBuger + minJuice - 50)
