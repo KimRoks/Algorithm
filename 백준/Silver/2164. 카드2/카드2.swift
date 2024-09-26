c90ec05fcf2c2e283c4c73f36e970e7fb3a923e8
KimRoks <113083860+KimRoks@users.noreply.github.com>
2024-09-26 14:45:08 +0900

    [Silver IV] Title: 카드2, Time: 36 ms, Memory: 77176 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.swift
@@ -0,0 +1,43 @@
+struct Q {
+    var input: [Int] = []
+    private var output: [Int] = []
+    
+    var isEmpty: Bool {
+        return input.isEmpty && output.isEmpty
+    }
+    
+    var count: Int {
+        input.count + output.count
+    }
+    
+    mutating func enqueue(newElemnet: Int) {
+        input.append(newElemnet)
+    }
+    
+    mutating func dequeue() -> Int? {
+        if output.isEmpty {
+            output = input.reversed()
+            input = []
+        }
+        return output.popLast()
+    }
+}
+
+let n = Int(readLine()!)!
+
+var card = Q()
+
+for i in 1...n {
+    card.enqueue(newElemnet: i)
+}
+
+while card.count != 1 {
+    let _ = card.dequeue()
+    if let dd = card.dequeue() {
+        card.enqueue(newElemnet: dd)
+    }
+}
+print(card.dequeue()!)
+
+
+
