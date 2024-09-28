5ae0f55cdb30e24ef31ebd755f31f5823ba43e3f
KimRoks <113083860+KimRoks@users.noreply.github.com>
2024-09-28 22:18:34 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.swift
@@ -0,0 +1,12 @@
+while true {
+    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    if input == [0, 0] {
+        break
+    }
+    
+    if input[0] > input[1] {
+        print("Yes")
+    } else {
+        print("No")
+    }
+}
