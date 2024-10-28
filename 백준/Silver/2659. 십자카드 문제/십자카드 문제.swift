6495cc679d58bd4739b5cc69f2fbe5db4e0a7a92
KimRoks <113083860+KimRoks@users.noreply.github.com>
2024-10-28 14:02:16 +0900

    [Silver III] Title: 십자카드 문제, Time: 12 ms, Memory: 79520 KB -BaekjoonHub

---
+++ 백준/Silver/2659. 십자카드 문제/십자카드 문제.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+func getMinRotation(_ numbers: [Int]) -> Int {
+    let rotations = [
+        numbers[0] * 1000 + numbers[1] * 100 + numbers[2] * 10 + numbers[3],
+        numbers[1] * 1000 + numbers[2] * 100 + numbers[3] * 10 + numbers[0],
+        numbers[2] * 1000 + numbers[3] * 100 + numbers[0] * 10 + numbers[1],
+        numbers[3] * 1000 + numbers[0] * 100 + numbers[1] * 10 + numbers[2]
+    ]
+    return rotations.min()!
+}
+
+var clockNumbers = Set<Int>()
+for i in 1111...9999 {
+    let temp = [i / 1000, (i / 100) % 10, (i / 10) % 10, i % 10]
+    if temp.contains(0) {
+        continue
+    }
+    let minRotation = getMinRotation(temp)
+    clockNumbers.insert(minRotation)
+}
+
+let sortedClockNumbers = Array(clockNumbers).sorted()
+
+var input = readLine()!.split(separator: " ").map { Int($0)! }
+let myMinClockNumber = getMinRotation(input)
+
+if let position = sortedClockNumbers.firstIndex(of: myMinClockNumber) {
+    print(position + 1)
+}
