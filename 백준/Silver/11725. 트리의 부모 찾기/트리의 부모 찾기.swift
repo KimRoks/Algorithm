fabbdca89c5036057c18bd0e22e241105e5f61ca
KimRoks <113083860+KimRoks@users.noreply.github.com>
2024-12-02 00:13:08 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 224 ms, Memory: 87516 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.swift
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.swift
@@ -1,30 +1,37 @@
+import Foundation
+
 let n = Int(readLine()!)!
 
 var graph = [[Int]](repeating: [], count: n + 1)
-
 for _ in 0..<n - 1 {
-    let input = readLine()!.split(separator: " ").map{Int($0)!}
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
     let (a, b) = (input[0], input[1])
     graph[a].append(b)
     graph[b].append(a)
 }
 
+var parent = Array(repeating: 0, count: n + 1) 
 var isVisited = Array(repeating: false, count: n + 1)
 
-var parentArray = Array(repeating: 0, count: n + 1)
+func dfs(start: Int) {
+    var stack = [start]
+    isVisited[start] = true
 
-func dfs(_ node: Int) {
-    isVisited[node] = true
-    for nextNode in graph[node] {
-        if !isVisited[nextNode] {
-            parentArray[nextNode] = node
-            dfs(nextNode)
+    while !stack.isEmpty {
+        let node = stack.removeLast() 
+
+        for nextNode in graph[node] {
+            if !isVisited[nextNode] {
+                isVisited[nextNode] = true
+                parent[nextNode] = node 
+                stack.append(nextNode) 
+            }
         }
     }
 }
 
-dfs(1)
+dfs(start: 1)
 
-for i in 2..<n + 1{
-    print(parentArray[i])
+for i in 2...n {
+    print(parent[i])
 }
