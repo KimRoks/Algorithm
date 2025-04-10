11d453ee208c94e7a3c5f8904f100b246b114b9b
KimRoks <113083860+KimRoks@users.noreply.github.com>
2025-04-10 10:58:04 +0900

    [Silver IV] Title: 대칭 차집합, Time: 212 ms, Memory: 93644 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.swift
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.swift
@@ -2,14 +2,9 @@ let nm = readLine()!.split(separator: " ").map{Int($0)!}
 
 let a = Set(readLine()!.split(separator: " ").map{Int($0)!})
 
-var resultA = a
-
 let b = Set(readLine()!.split(separator: " ").map{Int($0)!})
 
-var resultB = b
+let aCount = a.subtracting(b).count
+let bCount = b.subtracting(a).count
 
-resultA.subtract(resultB)
-
-resultB.subtract(a)
-
-print(resultA.count + resultB.count)
+print(aCount + bCount)
